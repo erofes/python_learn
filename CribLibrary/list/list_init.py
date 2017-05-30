b60f57c94d0d8c58d33aee0c420e8e1705deee4a
Kirill <erofes@mail.ry>
2017-05-30 21:21:08 +0700

    Добавлен свой метод

--- CribLibrary/list/list_init.py
+++ CribLibrary/list/list_init.py
@@ -13,3 +13,11 @@ print(second_list)
 third_list = [c * 3 for c in 'list'] #Using generator to create list
 print(third_list)
 #['lll', 'iii', 'sss', 'ttt']
+
+c = [c + d for c in '123' if c != '2' for d in 'abc' if d != 'b'] #example from site
+print(c)
+#['1a', '1c', '3a', '3c']
+
+fourth_list = [ [x, y, x + y] for x in range(1, 4) for y in range(1, 4)] #self-made
+print(fourth_list)
+#[[1, 1, 2], [1, 2, 3], [1, 3, 4], [2, 1, 3], [2, 2, 4], [2, 3, 5], [3, 1, 4], [3, 2, 5], [3, 3, 6]]
